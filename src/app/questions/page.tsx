c18dacc4f92a1c6b2cafb60c82ec16cd66c5e481
Gaurav Singh <125201478+gauravsingh906@users.noreply.github.com>
2024-11-26 02:20:03 +0530

    Update page.tsx

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 import { databases, users } from "@/models/server/config";
 import { answerCollection, db, voteCollection, questionCollection } from "@/models/name";
 import { Query } from "node-appwrite";
