dcdc94e4de7c4efcd5100bec586adb32e2909ae8
Gaurav Singh <125201478+gauravsingh906@users.noreply.github.com>
2024-11-26 01:45:00 +0530

    Update page.tsx

--- src/app/users/[userId]/[userSlug]/edit/page.tsx
+++ src/app/users/[userId]/[userSlug]/edit/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Page = () => {
     return (
         <div className="container mx-auto space-y-4 px-4 pb-20 pt-32">
-            <h1>work in progress</h1>
+            <h1>work in progress.....</h1>
 
         </div>
     );
